483b3a7be90f53ccc02ed9d15524270026237479
rodentia6 <rodentia6@gmail.com>
2019-07-02 10:36:23 +0900

    viewModelの生成方法を変更

--- app/src/main/java/com/rodentia6/sampledatabinding/MyActivity.kt
+++ app/src/main/java/com/rodentia6/sampledatabinding/MyActivity.kt
@@ -1,6 +1,6 @@
 package com.rodentia6.sampledatabinding
 
-import android.arch.lifecycle.ViewModelProviders
+import android.arch.lifecycle.ViewModelProvider
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -12,7 +12,14 @@ class MyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.my_activity)
 
-        val viewModel: MyViewModel = ViewModelProviders.of(this).get(MyViewModel::class.java)
+        // ViewModelオブジェクトを生成する
+        val viewModelFactory: ViewModelProvider.Factory = ViewModelProvider.NewInstanceFactory()
+        val viewModelProvider = ViewModelProvider(this@MyActivity, viewModelFactory)
+        val viewModel: MyViewModel = viewModelProvider.get(MyViewModel::class.java)
+        // 上記3行のコードは、build.gradleにimplementation 'android.arch.lifecycle:extensions:1.1.1'を追加すると下記で代替できるようになる
+        // val viewModel: MyViewModel = ViewModelProviders.of(this).get(MyViewModel::class.java)
+
+        // Bindingオブジェクトを生成する
         val binding: MyActivityBinding = DataBindingUtil.setContentView(this, R.layout.my_activity)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
